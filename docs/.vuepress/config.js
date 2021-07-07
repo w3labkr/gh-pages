993d95f224039f15438933c46daaee09d23bc91b
www <git@w3lab.kr>
2021-07-08 06:49:18 +0900

    1.0.0

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,49 @@
+module.exports = {
+  base: "/gh-pages/",
+  title: "VueJS",
+  description: "Frequently used vuepress settings.",
+  themeConfig: {
+    nav: [
+      { text: "Home", link: "/" },
+      { text: "Installation", link: "/#installation" },
+      { text: "Configure", link: "/#configure" },
+      { text: "Deploying", link: "/#deploying" },
+      { text: "Travis CI", link: "/#travis-ci" },
+      { text: "Github", link: "https://github.com/w3labkr/vuepress" },
+    ],
+    sidebar: "auto",
+    search: true,
+    searchMaxSuggestions: 10,
+    searchPlaceholder: "Search...",
+    lastUpdated: "Last Updated",
+    nextLinks: true,
+    prevLinks: true,
+    smoothScroll: true,
+  },
+  plugins: [
+    [
+      "@vuepress/active-header-links",
+      {
+        sidebarLinkSelector: ".sidebar-link",
+        headerAnchorSelector: ".header-anchor",
+      },
+    ],
+    ["@vuepress/back-to-top", true],
+    ["@vuepress/last-updated"],
+    ["@vuepress/medium-zoom"],
+    ["@vuepress/nprogress"],
+    ["@vuepress/pagination"],
+    [
+      "@vuepress/search",
+      {
+        searchMaxSuggestions: 10,
+      },
+    ],
+    [
+      "@vuepress/google-analytics",
+      {
+        ga: "UA-178473923-2",
+      },
+    ],
+  ],
+};
